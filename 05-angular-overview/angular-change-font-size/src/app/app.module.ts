fe1e3e3da55a70e32a9f249f4a9213d9fa24af9c
Toan Nguyen <toannguyenhue92@gmail.com>
2020-07-11 10:29:03 +0700

    angular databiding

--- 05-angular-overview/angular-change-font-size/src/app/app.module.ts
+++ 05-angular-overview/angular-change-font-size/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetComponent } from './pet/pet.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
-    PetComponent
+    PetComponent,
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule
